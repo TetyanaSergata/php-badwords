f2308cc9157f046270761d776746c7d8012a9f29
Tetyana <tsergata@gmail.com>
2020-02-24 09:20:03 +0100

    Snack fatto

---
+++ index.php
@@ -0,0 +1,30 @@
+<!-- Creare una variabile con un paragrafo di testo.
+Visualizzare a schermo il paragrafo con la relative lunghezza e sostituire la badword passata in GET con tre *.  -->
+
+<?php
+  $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  // var_dump($text);
+  $change = 'dolor';
+  ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Snack_PHP_Badwords</title>
+   </head>
+   <body>
+     <p>
+       <?php echo $text; ?>
+     </p>
+     <p>
+       Paragraph length: <?php echo strlen($text); ?> characters
+     </p>
+     <p>
+       <?php echo str_replace($change, '****', $text); ?>
+     </p>
+     <p>
+       Paragraph length: <?php echo strlen($text); ?> characters
+     </p>
+   </body>
+ </html>
